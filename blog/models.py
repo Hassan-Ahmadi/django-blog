478eecc7722241dcd8a3926f703fef262b467679
Hassan Ahmadi <h20.ahmadi@gmail.com>
2024-01-20 21:04:30 +0330

    created an initail model for Post in blog app

--- blog/models.py
+++ blog/models.py
@@ -3,5 +3,13 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
+    # image
     title = models.CharField(max_length=255)
     content = models.TextField()
+    # category
+    # author
+    counted_view = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(null=True)
