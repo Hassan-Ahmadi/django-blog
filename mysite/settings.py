f29006e98d7cc80c1c7d67d6c2c7440530361b27
Hassan Ahmadi <h20.ahmadi@gmail.com>
2024-01-20 21:11:03 +0330

    added website app, added Contact model to it

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     # my apps
-    'blog.apps.BlogConfig'
+    'blog.apps.BlogConfig',
+    'website.apps.WebsiteConfig'
 ]
 
 MIDDLEWARE = [
