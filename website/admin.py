7e77c37e5c94e69b0d0194c560b17012d9f51ac8
Hassan Ahmadi <h20.ahmadi@gmail.com>
2024-01-20 21:48:48 +0330

    added contact to admin panel with some customization

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Contact
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'subject', 'message', 'created_at')
+    ordering = ('-created_at',)
+    search_fields = ('subject', 'message')
+    
