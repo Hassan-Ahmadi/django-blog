776df42b6f70fcf25bb706b47f28ea48cfd88f5a
Hassan Ahmadi <h20.ahmadi@gmail.com>
2024-01-22 01:58:44 +0330

    added next and previouse posts to single post view

--- blog/views.py
+++ blog/views.py
@@ -12,10 +12,33 @@ def blog_view(request):
     return render(request, 'blog/index.html', context)
 
 def blog_single(request, pid: int):
+    context = {}
+    
     post = get_object_or_404(Post, pk=pid)
     post.counted_view += 1
     post.save()
-    context = {'post': post}
+    
+    try:
+        next_post = Post.objects.get(id=pid + 1)
+        
+        if not next_post.is_published:
+            next_post = None
+            
+    except Post.DoesNotExist:
+        next_post = None
+        
+    try:        
+        pre_post = Post.objects.get(id=pid-1)        
+        
+        if not pre_post.is_published:
+            pre_post = None
+            
+    except Post.DoesNotExist:
+        pre_post = None
+    
+    context = {'post': post,
+               'next_post': next_post,
+               'pre_post': pre_post}
     return render(request, 'blog/blog-single.html', context)
 
 def test_view(request):
