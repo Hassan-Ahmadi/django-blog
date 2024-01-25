99002a2ef6994c990a730abc7748260e82bfb49e
Hassan Ahmadi <h20.ahmadi@gmail.com>
2024-01-25 14:05:05 +0330

    added date filter to pages

--- blog/views.py
+++ blog/views.py
@@ -1,46 +1,62 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
+
 # import datetime
 # Create your views here.
 
+
 def blog_view(request):
     # posts = Post.objects.filter(is_published=True)
-    
+
     posts = Post.objects.filter(published_date__lte=timezone.now(), is_published=True)
-    context = {'posts': posts}    
-    return render(request, 'blog/blogs.html', context)
+    context = {"posts": posts}
+    return render(request, "blog/blogs.html", context)
+
 
 def blog_single(request, pid: int):
     context = {}
-    posts = Post.objects.filter(pk=pid, is_published=True)
+    posts = Post.objects.filter(
+        pk=pid, is_published=True, published_date__lte=timezone.now()
+    )
     post = get_object_or_404(posts)
     post.counted_view += 1
     post.save()
-    
+
     try:
-        next_post = Post.objects.filter(id__gt=post.id, is_published=True, published_date__lte=timezone.now()).order_by('id').first()
-        if next_post:                
+        next_post = (
+            Post.objects.filter(
+                id__gt=post.id, is_published=True, published_date__lte=timezone.now()
+            )
+            .order_by("id")
+            .first()
+        )
+        if next_post:
             if not next_post.is_published:
                 next_post = None
-            
+
     except Post.DoesNotExist:
         next_post = None
-        
-    try:        
-        pre_post = Post.objects.filter(id__lt=post.id, is_published=True, published_date__lte=timezone.now()).order_by('-id').first()
-        
+
+    try:
+        pre_post = (
+            Post.objects.filter(
+                id__lt=post.id, is_published=True, published_date__lte=timezone.now()
+            )
+            .order_by("-id")
+            .first()
+        )
+
         if pre_post:
             if not pre_post.is_published:
                 pre_post = None
-            
+
     except Post.DoesNotExist:
         pre_post = None
-    
-    context = {'post': post,
-               'next_post': next_post,
-               'pre_post': pre_post}
-    return render(request, 'blog/blog-single.html', context)
+
+    context = {"post": post, "next_post": next_post, "pre_post": pre_post}
+    return render(request, "blog/blog-single.html", context)
+
 
 def test_view(request):
-    return render(request, 'website/404-page.html')
+    return render(request, "website/404-page.html")
