c9956fca99fe6af2153cfc1a91cf04f87fa14e89
Hassan Ahmadi <h20.ahmadi@gmail.com>
2024-02-25 20:12:15 +0330

    Refactor message display in base.html and add Bootstrap to test.html

---
+++ statics/assets/js/toast.js
@@ -0,0 +1,10 @@
+// toast.js
+
+// Get the toast element
+const toastElement = document.querySelector('.toast');
+
+// Create a new Bootstrap toast instance
+const toast = new bootstrap.Toast(toastElement);
+
+// Show the toast
+toast.show();
